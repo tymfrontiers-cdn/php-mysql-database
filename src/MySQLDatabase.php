8ba4970abc9f7f07d5deef677ec1d6d1022428f6
Sir Louis <me@sirlouis.io>
2020-01-13 16:23:19 +0100

    bug fix
    Magic quote removed

--- src/MySQLDatabase.php
+++ src/MySQLDatabase.php
@@ -7,8 +7,6 @@ class MySQLDatabase{
   private    static    $_db_pass;
   private    static    $_db_name;
   private        			 $_connection;
-  private              $_magic_quotes_acitive;
-  private              $_real_escape_string_exists;
 
   public  						 $last_query;
   public 							 $errors = [];
@@ -30,8 +28,6 @@ class MySQLDatabase{
     self::$_db_pass   = $this->escapeValue($db_pass);
     self::$_db_name   = $this->escapeValue($db_name);
     $this->openConnection();
-    $this->_magic_quotes_acitive = get_magic_quotes_gpc();
-    $this->_real_escape_string_exists = function_exists("mysqli_real_escape_string");
   }
   public function dbName(){ return self::$_db_name; }
   // open Database connection using given params
@@ -40,11 +36,11 @@ class MySQLDatabase{
       new \mysqli(self::$_db_server, self::$_db_user, self::$_db_pass, self::$_db_name) :
       new \mysqli(self::$_db_server, self::$_db_user, self::$_db_pass);
 
-    if (mysqli_connect_error()) {
+    if (\mysqli_connect_error()) {
       // error everyone can see
       $this->errors['openConnection'][] =[0,256,"Failed to connect to database.",__FILE__,__LINE__];
       // error for admin only
-      $err_arr = error_get_last();
+      $err_arr = \error_get_last();
       $this->errors['openConnection'][] = [3,$err_arr['type'],$err_arr["message"],$err_arr['file'],$err_arr['line']];
       return false;
     }else{
@@ -79,7 +75,7 @@ class MySQLDatabase{
 	public function changeDB(string $db_name){
 		if( $db_name && $db_name !== self::$_db_name ){
 			if( !$this->_connection->select_db($db_name) ){
-        $err_arr = error_get_last();
+        $err_arr = \error_get_last();
         if($err_arr)  $this->errors['changeDB'][] = [3,$err_arr['type'],$err_arr["message"],$err_arr['file'],$err_arr['line']];
         return false;
       }else{ return true; }
@@ -91,15 +87,9 @@ class MySQLDatabase{
 	public function fetchAll($result_set){ return $result_set->fetch_all(); }
 
   public function escapeValue($value){
-    if($this->_real_escape_string_exists){// PHP 4.3.0 or higher
-			// undo any magic quote effects so mysql_real_escape_string can do the work
-			if($this->_magic_quotes_acitive){ $value = stripcslashes($value);}
-			$value = mysqli_real_escape_string($this->_connection,$value);
-		} else{ // before PHP 4.3.0
-			// if magic quotes aren't already on then add slashes manually
-			if( !$this->_magic_quotes_acitive ){ $value = addcslashes($value,"\0..\37!@\177..\377");}
-			// if magic quotes are active then slashes already exist
-		}
+    $value = $this->_connection
+      ? \mysqli_real_escape_string($this->_connection,$value)
+      : \addslashes($value);
 		return $value;
   }
 
